b13b4680f4753f6b7d8d3bec866ea37f4dcc4786
github username <github email address>
2019-09-04 07:06:10 +0100

    Done.

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,39 +1,41 @@
 class Guest
-# has many trips
-# has many guests thorugh trips
+    # has many trips
+    # has many listings through trips
 
-    attr_reader :name
-
+    attr_reader :name # we need to read those by .notsation later
+ 
     @@all = []
 
     def initialize(name)
         @name = name
-        @@all << self
+        @@all << self # name goes in @@all
     end
 
     def self.all
         @@all
     end
 
-    def trips
-        Trip.all.select {|t| t.guest == self}
+    def listings
+        self.trips.map {|t| t.listing}
     end
 
-    def guests
-        self.trips.map {|t| t.listing}
+    def trips
+        Trip.all.select {|t| t.guest == self}
     end
 
     def trip_count
         trips.length
     end
 
-    def self.pro_traveller
+    def self.pro_taveller
         @@all.select {|g| g.trip_count > 1}
-        
     end
 
-    def self.find_all_by_name(name)
-        @@all.select {|g| g.name == name}
+    def self.find_all_by_city(name)
+        @@all.select {|g| g.name == name} # here, only methods available in this class can be used.
     end
 
 end
+
+
+
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,19 +1,19 @@
 class Trip
-
-    attr_reader :guest, :listing
-
+    # belongs to a guest
+    # belongs to a listing
+ 
+    attr_reader :guest, :listing # we need to read those by .notsation later
+ 
     @@all = []
 
-    def initialize(guest, listing)
+    def initialize(guest, listing)  #city argument=attribute is available in Listing so no need to add city as an argument in Trip
         @guest = guest
         @listing = listing
-        @@all << self
-        
+        @@all << self # guest and listing go in @@all
     end
 
     def self.all
         @@all
     end
-
 
 end
