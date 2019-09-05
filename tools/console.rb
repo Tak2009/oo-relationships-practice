a316b1fc69daa7a975bc5539643b6eac7f609bf3
github username <github email address>
2019-09-05 22:41:19 +0100

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -6,7 +6,16 @@ end
 
 # start here
 
+d1 = Dessert.new("Cake")
+d2 = Dessert.new("Cake2")
+d3 = Dessert.new("Cake3")
+d4 = Dessert.new("Cake4")
+d5 = Dessert.new("Cake5")
 
+b1 = Bakerhy.new("AAAA")
+b2 = Bakerhy.new("BB")
+b3 = Bakerhy.new("CCCCCCCC")
+b4 = Bakerhy.new("DDD")
 
 binding.pry
 "Start"
