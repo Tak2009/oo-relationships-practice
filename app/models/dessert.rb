8208570fb6178376134e34124eef753936541975
github username <github email address>
2019-09-05 18:50:46 +0100

    Done.

---
+++ app/models/dessert.rb
@@ -0,0 +1,19 @@
+class Dessert
+
+    attr_reader :name, 
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def ingredients
+    end
+
+end
---
+++ app/models/ingredient.rb
@@ -0,0 +1,19 @@
+class Ingredients
+
+    attr_reader :name, :calorie, :dessert, :bakery, 
+
+    @@all = []
+
+    def initialize(name, calorie, dessert, bakery)
+        @name = name
+        @calorie = calorie
+        @dessert = dessert
+        @bakery = bakery
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
